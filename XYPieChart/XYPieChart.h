09547e944d6938548ffaa7f80db8328571a55a55
XY Feng <fxy0111@gmail.com>
2012-03-06 22:31:06 -0500

    add license text

--- XYPieChart/XYPieChart.h
+++ XYPieChart/XYPieChart.h
@@ -5,10 +5,33 @@
 //  Created by XY Feng on 2/24/12.
 //  Copyright (c) 2012 Xiaoyang Feng. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
 @class XYPieChart;
+
+#pragma mark - Data Source Protocol
+//Provide value and color for each slice
 @protocol XYPieChartDataSource <NSObject>
 @required
 - (NSUInteger)numberOfSlicesInPieChart:(XYPieChart *)pieChart;
@@ -17,6 +40,8 @@
 - (UIColor *)pieChart:(XYPieChart *)pieChart colorForSliceAtIndex:(NSUInteger)index;
 @end
 
+#pragma mark - Delegate Protocol
+//Method called before and after one slice get selected
 @protocol XYPieChartDelegate <NSObject>
 @optional
 - (void)pieChart:(XYPieChart *)pieChart willSelectSliceAtIndex:(NSUInteger)index;
@@ -25,6 +50,7 @@
 - (void)pieChart:(XYPieChart *)pieChart didDeselectSliceAtIndex:(NSUInteger)index;
 @end
 
+#pragma mark - Pie Chart 
 @interface XYPieChart : UIView
 @property(nonatomic, weak) id<XYPieChartDataSource> dataSource;
 @property(nonatomic, weak) id<XYPieChartDelegate> delegate;
